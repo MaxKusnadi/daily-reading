38dd5b3c9b1213031dce9408a7e483cc794482e0
Max Kusnadi <max.kusnadi@gmail.com>
2017-09-22 00:11:17 +0800

    Finished all

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 git pull
 kill $(ps aux | grep 'daily-reading' | awk '{print $2}')
-nohup gunicorn --certfile cert.pem --keyfile key.pem -k gevent -b 0.0.0.0:6000 --worker-connections 10 wsgi:app &
+nohup gunicorn --certfile cert.pem --keyfile key.pem -k gevent -b 0.0.0.0:3010 --worker-connections 10 wsgi:app &
